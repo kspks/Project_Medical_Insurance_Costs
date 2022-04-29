9e2eb971098d21ea21af53f03d1e0eba4523d7bc
Kseniia <90192541+kspks@users.noreply.github.com>
2022-04-29 13:31:14 +0200

    Update and rename U.S. Medical Insurance Costs.py to Medical Insurance Costs.py

--- Medical Insurance Costs.py
+++ Medical Insurance Costs.py
@@ -2,7 +2,6 @@
 # coding: utf-8
 
 # # U.S. Medical Insurance Costs
-# 
 # For this project, I was investigating a medical insurance costs dataset in a insurance.csv file using the Python skills. 
 # 
 # File **insurance.csv** contains the following columns:
@@ -15,12 +14,8 @@
 # * Patient Yearly Medical Insurance Cost
 # 
 # There are no signs of missing data. To store this information, seven empty lists will be created hold each individual column of data from **insurance.csv**.
-# 
 
-# In[431]:
-
-
-# import csv library
+# Import csv library
 import csv
 import math
 import pandas as pd
@@ -28,10 +23,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import seaborn as sns
-
-
-# In[432]:
-
 
 #Create empty lists for the various attributes in insurance.csv
 ages = []
@@ -42,13 +33,9 @@ smoker_statuses = []
 regions = []
 insurance_charges = []
 
-
 # The helper function below was created to make loading data into the lists as efficient as possible. Without this function, one would have to open **insurance.csv** and rewrite the `for` loop seven times; however, with this function, one can simply call `load_list_data()` each time as shown below.
 
-# In[433]:
-
-
-# helper function to load csv data
+# Helper function to load csv data
 def load_list_data(lst, csv_file, column_name):
     # open csv file
     with open(csv_file) as csv_info:
@@ -61,11 +48,7 @@ def load_list_data(lst, csv_file, column_name):
         # return the list
         return lst
 
-
-# In[434]:
-
-
-# look at the data in insurance_csv_dict
+# Let's look at the data in insurance_csv_dict
 load_list_data(ages, 'insurance.csv', 'age')
 load_list_data(sexes, 'insurance.csv', 'sex')
 load_list_data(bmis, 'insurance.csv', 'bmi')
@@ -74,14 +57,13 @@ load_list_data(smoker_statuses, 'insurance.csv', 'smoker')
 load_list_data(regions, 'insurance.csv', 'region')
 load_list_data(insurance_charges, 'insurance.csv', 'charges')
 
-
 # Now that all the data from **insurance.csv** neatly organized into labeled lists, the analysis can be started. For analysis our data, let's inplement the next operations:
 # * show average age of the patients
 # * write the number of males vs. females counted in the dataset
 # * find geographical location of the patients
 # * return the average yearly medical charges of the patients
 # * creating a dictionary that contains all patient information
-# 
+ 
 # To perform these inspections, a class called `PatientsInfo` has been built out which contains fives methods:
 # * `analyze_ages()`
 # * `analyze_sexes()`
@@ -92,12 +74,9 @@ load_list_data(insurance_charges, 'insurance.csv', 'charges')
 # 
 # The class has been built out below. 
 
-# In[435]:
-
-
 class PatientsInfo:
     
-    # init method that takes in each list parameter
+    # Init method that takes in each list parameter
     def __init__(self, patients_ages, patients_sexes, patients_bmis, patients_num_children, 
                  patients_smoker_statuses, patients_regions, patients_charges):
         self.patients_ages = patients_ages
@@ -108,7 +87,7 @@ class PatientsInfo:
         self.patients_regions = patients_regions
         self.patients_charges = patients_charges
  
-    # method that calculates the average ages of the patients
+    # Method that calculates the average ages of the patients
     def analyze_ages(self):
         # initialize total age at zero
         total_age = 0
@@ -120,7 +99,7 @@ class PatientsInfo:
         # return total age divided by the length of the patient list
         return ("Average Patient Age: " + str(avg_age) + " years")
     
-    # method that calculates the number of males and females
+    # Method that calculates the number of males and females
     def analyze_sexes(self):
         females = 0
         males = 0
@@ -140,7 +119,7 @@ class PatientsInfo:
         print ("Percentage of male patients: " + str(round(male_perc, 2)) + "%." )
         print ("Percentage of female patients: " + str(round(female_perc, 2)) + "%.")
 
-    # method to find each unique region patients are from
+    # Method to find each unique region patients are from
     def patient_regions(self):
         patient_region = []
         for region in self.patients_regions:
@@ -151,7 +130,7 @@ class PatientsInfo:
         # return unique regions list
         return ("The regions all the patients are from: " + str(patient_region))
 
-    # method to find average yearly medical charges for patients
+    # Method to find average yearly medical charges for patients
     def calc_average_med_charges(self):
         total_charges = 0
         # iterate through charges in patients charges list
@@ -162,7 +141,7 @@ class PatientsInfo:
         return ("Average Yearly Medical Insurance Charges: " +  
                 str(average_med_charges) + " dollars.")
 
-     # method to calculate the maximal insurance charge 
+     # Method to calculate the maximal insurance charge 
     def calc_max_charge(self):
         max_charge = 0
         for charge in self.patients_charges:
@@ -171,7 +150,7 @@ class PatientsInfo:
         return ("Max Yearly Medical Insurance Charges: " +  
                 str(round(max_charge, 2)) + " dollars.")
     
-    # method to calculate the minimal insurance charge 
+    # Method to calculate the minimal insurance charge 
     def calc_min_charge(self):
         min_charge = float(self.patients_charges[0])
         for j in range(len(self.patients_charges)):
@@ -179,10 +158,11 @@ class PatientsInfo:
                 min_charge = float(self.patients_charges[j])
         return ("Min Yearly Medical Insurance Charges: " +  
                 str(round(min_charge, 2)) + " dollars.")
-    
+   
+    # Implement new stastical parameter
     z_value = 1.960 
     
-    # method to calculate the range insurance charge 
+    # Method to calculate the range insurance charge 
     def range_insurance_charges(self):
         max_charge = min_charge = float(self.patients_charges[0])
         # calculate the minimal insurance charge 
@@ -190,7 +170,7 @@ class PatientsInfo:
             if   (min_charge > float(self.patients_charges[j])) :
                 min_charge = float(self.patients_charges[j])
                 
-         # calculate the maximal insurance charge       
+     # Calculate the maximal insurance charge       
         for j in range(len(self.patients_charges)):
             if   (max_charge < float(self.patients_charges[j])) :
                 max_charge = float(self.patients_charges[j])
@@ -204,7 +184,6 @@ class PatientsInfo:
         sum_x_values = 0.00
         for i in range(len(x)):
             sum_x_values += float(x[i])
-            
         return(round(sum_x_values / len(x), 2))
     
     @staticmethod
@@ -212,15 +191,13 @@ class PatientsInfo:
         mu = patient_data.mean(x)
         x2 = [(float(j)-mu) ** 2 for j in x]   
         m = round(sum(x2) / len(x2), 2)
-    
         return(round(math.sqrt(m), 2))
     
     @staticmethod
     def margin_error(x : list):
         s = patient_data.std(x)
         z = patient_data.z_value
-        me = round(z * s /math.sqrt(len(x)), 2)
-                   
+        me = round(z * s /math.sqrt(len(x)), 2)            
         return(me)
     
     def insurance_charges_statistics(self):
@@ -246,7 +223,7 @@ class PatientsInfo:
         print ("And the Margin Error for a conficence interval of 95% is " + str(margin_error))
     
     
-    # method to create dictionary with all patients information
+    # Method to create dictionary with all patients information
     def create_patient_dictionary(self):
         # initialize empty dictionary
         patient_information = {}
@@ -260,119 +237,39 @@ class PatientsInfo:
         patient_information["Region"] = self.patients_regions
         patient_information["Charge"] = self.patients_charges
         return patient_information
-   
-    
-    
-    
-    
-
 
 #  The next step is to create an instance of the class called `patient_data`. With this instance, each method can be used to see the results of the analysis.
 
-# In[436]:
-
-
 patient_data = PatientsInfo(ages, sexes, bmis, num_children, smoker_statuses, regions, insurance_charges)
-
-
-# In[437]:
-
-
 patient_data.analyze_ages()
-
-
 # So, the average age of the patients is about 39 years old.
 
-# In[438]:
-
-
 patient_data.analyze_sexes()
-
-
 # The balance between men and women shows that we have an almost equal number of both sexes.
 
-# In[439]:
-
-
 patient_data.patient_regions()
-
-
 # There are four unique geographical regions in this dataset.
 
-# In[440]:
-
-
 patient_data.calc_average_med_charges()
-
-
-# In[441]:
-
-
 patient_data.calc_max_charge()
-
-
-# In[442]:
-
-
 patient_data.calc_min_charge()
-
-
-# In[443]:
-
-
 patient_data.range_insurance_charges()
-
-
-# In[444]:
-
-
 patient_data.create_patient_dictionary()
 
 
 # All patient data is now neatly organized in a dictionary. This is convenient for further analysis.
-
 # In this step need to introduce the `z_value` for the Confidence Interval. For example, for a Confidence Interval of 95%, it's Z value is 1.960. And using this parameter we can calculate Margin Error for Confidence Interval of 95% = [12,621.77  13,919.07].
 
-# In[445]:
-
-
 patient_data.insurance_charges_statistics()
-
-
-# In[446]:
-
-
 patient_data.mean(insurance_charges)
-
-
-# In[447]:
-
-
 patient_data.std(insurance_charges)
-
-
-# In[448]:
-
-
 patient_data.margin_error(insurance_charges)
 
-
 # **Grouping**
-
-# In[449]:
-
-
 df = pd.DataFrame(patient_data.create_patient_dictionary())
-
 print(df.head(11))
 
-
 # **Group by Sex, get mean, min, and max value of Age for each value of Sex.**
-# 
-
-# In[450]:
-
-
 age_by_sex = df.groupby('Sex').agg({'Age': ['mean', 'min', 'max']})
 
 # rename columns
@@ -380,15 +277,9 @@ age_by_sex.columns = ['age_mean', 'age_min', 'age_max']
 
 # reset index to get grouped columns back
 age_by_sex = age_by_sex.reset_index()
-
 print(age_by_sex)
 
-
 # **Group by Sex and Region, get mean, min, and max value of the medical insurance cost for each value of Sex.**
-
-# In[451]:
-
-
 # convert Charge data type to numeric using pd
 df['Charge'] = pd.to_numeric(df['Charge'])
 df['BMI'] = pd.to_numeric(df['BMI'])
@@ -402,32 +293,16 @@ charge_by_sex_region.columns = ['charge_mean', 'charge_min', 'charge_max']
 
 # reset index to get grouped columns back
 charge_by_sex_region = charge_by_sex_region.reset_index()
-
 print(charge_by_sex_region)
 
-
 # **Select top 5 insurance charges grouped by sex and region**
-# 
-
-# In[452]:
-
 
 sr = ['Sex', 'Region']
 top5 = df.groupby(sr)['Charge'].nlargest(5)
 print(top5)
 print(type(top5))
 
-
 # **Diagram Age vs Charge of Insurance**
-
-# In[ ]:
-
-
-
-
-
-# In[540]:
-
 
 fg = plt.figure(figsize=(11, 4), constrained_layout=True)
 gs = gridspec.GridSpec(ncols=2, nrows=1, figure=fg)
@@ -449,18 +324,7 @@ plt.plot(df.BMI, line_function(df.BMI), "y--")
 plt.xlabel('IBM')
 plt.ylabel('Amount of Charge')
 plt.title('IBM vs Charge of Insurance')
-
 plt.show()
-
-
-# In[ ]:
-
-
-
-
-
-# In[533]:
-
 
 plt.scatter(df.NumberOfChildren, df.Charge)
 z = np.polyfit(df.NumberOfChildren, df.Charge, 1)
@@ -471,11 +335,7 @@ plt.ylabel('Amount of Charge')
 plt.title('Number of Children vs Charge of Insurance')
 plt.show()
 
-
 # **Histogram**
-
-# In[542]:
-
 
 a = np.array(df['Charge'])
 fig, ax = plt.subplots(figsize =(6, 4))
@@ -485,11 +345,7 @@ plt.ylabel('Number Of Patients')
 plt.title('Histogram')
 plt.show()
 
-
 # **Patient Population Distribution over Regions**
-
-# In[503]:
-
 
 arrays = [df['Region'],
           df['Smoker']]
@@ -518,17 +374,7 @@ plt.setp(autotexts, size=8, weight="bold")
 ax.set_title("Patient Population Distribution over Regions")
 plt.show()
 
-
 # **Average medical insurance costs by type of attribut**
-
-# In[ ]:
-
-
-
-
-
-# In[513]:
-
 
 arrays = [df['Region'],
           df['Smoker']]
@@ -547,11 +393,7 @@ print(df.groupby(['Sex', 'Region']).mean()['Charge'])
 print(df.groupby(['Sex', 'Region']).std()['Charge'])
 plt.show()
 
-
 # **Diagram Age vs Insurance Charge with info about smoking and amount of children**
-
-# In[547]:
-
 
 arrays = [df['Smoker'],df['Age'], df['NumberOfChildren']]
 index = pd.MultiIndex.from_arrays(arrays, names=('Smoker', 'Age', 'NumberOfChildren'))
@@ -570,11 +412,7 @@ line_function = np.poly1d(z)
 plt.plot(avg_age_level['Age'], line_function(avg_age_level['Age']), "b--")
 plt.show()
 
-
 # **Diagram Age vs Insurance Charge with info about smoking and BMI**
-
-# In[545]:
-
 
 arrays = [df['Smoker'],df['Age'], df['NumberOfChildren'], df['BMI']]
 index = pd.MultiIndex.from_arrays(arrays, names=('Smoker', 'Age', 'Number Of Children', 'BMI'))
@@ -591,16 +429,4 @@ z = np.polyfit(avg_numchild_level['Number Of Children'], avg_numchild_level['Ins
 line_function = np.poly1d(z)
 plt.plot(avg_numchild_level['Number Of Children'], line_function(avg_numchild_level['Number Of Children']), "p--")
 plt.show()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
 
